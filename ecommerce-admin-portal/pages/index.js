eff78f22c17aa3043b03d2ee92e3de14b5a8c737
Ryushrestha <ryushrestha7@gmail.com>
2023-05-16 17:48:18 +0545

    feat:Navbar,login logout

--- ecommerce-admin-portal/pages/index.js
+++ ecommerce-admin-portal/pages/index.js
@@ -1,6 +1,35 @@
+import Layout from "@/components/Layout";
+import { useSession,signIn,signOut } from "next-auth/react"
+
 export default function Component() {
-  return (
-    <>Ecommerce Admin</>
-  )
+const {data : session} = useSession();
+
+// if (session){
+//   return (
+//     <>
+//     Signed in as {session.user.email}<br></br>
+//     <button onClick={()=>signOut()}>Sign Out</button>
+//     </>
+//   )
+// }
+// return (
+//   <>
+//   Signed Out
+//   <br></br>
+//   <button onClick={()=>signIn()}>Sign In</button>
+//   </>
+// )
+
+return (
+    <Layout>
+      <div className="text-blue-800 flex justify-between">
+        <h2>Hello, {session?.user?.name}</h2>
+        <div className="flex bg-gray-400 flex-col">
+          <img src={session?.user?.image}/>
+          <span>{session?.user?.email}</span>
+        </div>
+      </div>
+    </Layout>
+)
 }
 
